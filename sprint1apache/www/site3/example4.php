835990972998ada0eaf4e0e8e26b3263dce4f63d
IagoMC <113433698+IagoMC@users.noreply.github.com>
2022-09-29 12:03:02 +0200

    Add files via upload

---
+++ sprint1apache/www/site3/example4.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+        <h1>Jubilación</h1>
+       
+        <?php
+        function edad_en_5_años($edad) {
+            return $edad + 10;
+        } 
+          
+        function mensaje($age) {
+        if (edad_en_5_años($age) > 65) {
+            return "En 5 años tendrás edad de jubilación";
+        } else {
+            return "¡Disfruta de tu tiempo!";
+        }
+        }
+        ?>
+
+        <table>
+            <tr>
+                <th>Edad</th>
+                <th>Info</th>
+            </tr>
+            <?php
+            $lista = array(60,61,62,63,64);
+            foreach ($lista as $valor) {
+                echo "<tr>";
+                echo "<td>".$valor."</td>";
+                echo "<td>".mensaje($valor)."</td>";
+                echo "</tr>";
+            }
+            ?>
+
+        </table>
+    </body>
+</html>
