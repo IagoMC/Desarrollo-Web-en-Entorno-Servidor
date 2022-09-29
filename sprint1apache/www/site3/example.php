0a1b921069797ddd1400813d818874032306b8e3
iago <imoralesc@fpcoruna.afundacion.org>
2022-09-29 10:54:47 +0200

    PHP 1

--- sprint1apache/www/site3/example.php
+++ sprint1apache/www/site3/example.php
@@ -1,9 +1,9 @@
 <html>
     <body>
-            h1>El ganador es...</h1>
+          <h1>El ganador es...</h1>
         <?php
-        $NOMBRE = "Jaime";
+        $NOMBRE = "Thanos";
         echo $NOMBRE;
         ?>
-<   /body>
+</body>
 </html>
