2e78257fd1ebf8c39a0505b6f1b09733873be533
IagoMC <113433698+IagoMC@users.noreply.github.com>
2022-10-11 12:51:42 +0200

    Update detail.php

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -1,23 +1,26 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '1234', 'web_canciones') or die('Fail');
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
   <body>
   <?php
-    if (!isset($_GET['cancion_id'])) {
+    if (!isset($_GET['libro_id'])) {
       die('No se ha especificado una canción');
     }
-    $cancion_id = $_GET['cancion_id'];
-    $query = 'SELECT * FROM tCanciones WHERE id='.$cancion_id;
+    $cancion_id = $_GET['libro_id'];
+    $query = 'SELECT * FROM tLibros WHERE id='.$libro_id;
     $result = mysqli_query($db, $query) or die('Query error');
     $only_row = mysqli_fetch_array($result);
-    echo '<h1>'.$only_row['titulo'].'</h1>';
-    echo '<h2>'.$only_row['año'].'</h2>';
+    echo '<h1>'.$only_row['id'].'</h1>';
+    echo '<h2>'.$only_row['nonbre'].'</h2>';
+    echo '<h2>'.$only_row['genero'].'</h2>';
+    echo '<h2>'.$only_row['url_imagen'].'</h2>';
+    echo '<h2>'.$only_row['autor'].'</h2>';
     ?>
     <h3>Comentarios:</h3>
     <ul>
     <?php
-    $query2 = 'SELECT * FROM tComentarios WHERE cancion_id='.$cancion_id;
+    $query2 = 'SELECT * FROM tComentarios WHERE cancion_id='.$libro_id;
     $result2 = mysqli_query($db, $query2) or die('Query error');
     while ($row = mysqli_fetch_array($result2)) {
       echo '<li>'.$row['comentario'].'</li>';
