dc5e8bf19c6697d275e10a9bd399aba41aec79f7
IagoMC <113433698+IagoMC@users.noreply.github.com>
2022-10-15 12:31:23 +0200

    Add files via upload

---
+++ sprint2python/concurso.py
@@ -0,0 +1,19 @@
+print("Seleccione la respuesta correcta")
+print("¿Cual es el mejor equipo?")
+
+print("A - Barcelona")
+print("B - Real Madrid")
+print("C - Depor")
+respuesta=input()
+
+if respuesta=="A":  
+    print("Respuesta Correcta")
+
+elif respuesta=="a":
+    print("Respuesta Correcta")
+
+elif respuesta=="Barcelona":
+    print("Respuesta Correcta")
+
+else:
+    print("Respuesta Incorrecta")
