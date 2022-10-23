25772e7cc4e81dd6c2cf9f18741654e4afbe637b
Iago <imoralesc@fpcoruna.afundacion.org>
2022-10-23 22:03:18 +0200

    sesion7.5

--- sprint3sesiones/www/mysite/nc.php
+++ sprint3sesiones/www/mysite/nc.php
@@ -1,30 +1,33 @@
 <?php
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
-session_start();
-$email_posted = $_POST['f_email'];
-$password_posted = $_POST['f_password'];
-$Nueva = $_Post['f_nueva'];
-const v = 0;
-$query = "SELECT id, contraseña FROM tUsuarios WHERE email = '" . $email_posted . "'";
+
+$email_posted = $_POST['f_email2'];
+$password_posted = $_POST['f_password2'];
+$Nueva = $_Post['Nueva'];
+
+$query = "SELECT id, contraseña FROM tUsuarios WHERE email = '" . $email_posted2 . "'";
 $result = mysqli_query($db, $query) or die('Query error');
 
 if (mysqli_num_rows($result) > 0) {
     $only_row = mysqli_fetch_array($result);
-    if ($only_row[1] == $password_posted) {
-        session_start();
-	const v = 1;
-        $_SESSION['user_id'] = $only_row[0];
-        header('Location: main.php');
-    } else {
+    if ($only_row[1] == $password_posted2) {
+        //session_start();
+	
+        //$_SESSION['user_id'] = $only_row[0];
+	$query = "UPDATE tUsuarios SET contraseña = '".$Nueva. where email = .$email_posted2"'"; 
+	//echo 'confirmado';
+
+
+	//session_start();
+	//$_SESSION['user_id'] = $Nueva;
+        //header('Location: main.php');  
+	echo '<p>Confirmado</p>';  
+
+} else {
         echo '<p>Contraseña incorrecta</p>';
     }
 } else {
     echo '<p>Usuario no encontrado con ese email</p>';
-}
-
-if (v==1){
-	$query = "update table tComentarios SET contraseña =".$Nueva; 
-
 }
 
 ?>
