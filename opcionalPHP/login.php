c952c094c38fe7896d60ce127883c18953607e9b
Iago <imoralesc@fpcoruna.afundacion.org>
2022-11-24 10:39:04 +0100

    opcionalphpFinalizado

--- opcionalPHP/login.php
+++ opcionalPHP/login.php
@@ -6,8 +6,9 @@ $contrasena= $_POST['contraseña'];
 
 $query = "SELECT id, contrasena FROM usuarios WHERE nombre = '" . $usuario . "'";
 $result = mysqli_query($db, $query) or die('Query error');
-
-
+$only_row = mysqli_fetch_array($result);
+echo $only_row[1];
+echo 'aaa';
 if (password_verify($contrasena, $only_row[1])) {
     session_start();
     $_SESSION['user_id'] = $only_row[0];
