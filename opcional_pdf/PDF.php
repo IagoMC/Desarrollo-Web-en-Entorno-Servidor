aef9ed2dff7bfad0395152ae903a093192138fd0
Iago <imoralesc@fpcoruna.afundacion.org>
2022-12-13 13:31:58 +0100

    pdf5

--- opcional_pdf/PDF.php
+++ opcional_pdf/PDF.php
@@ -3,7 +3,28 @@ $nombre = $_POST['nombre'];
 $apellidos = $_POST['apellidos'];
 
 require('fpdf.php');
-$pdf = new FPDF();
+
+class PDF extends FPDF{
+    function Header(){
+        $this->Image('xunta.png',10,10,50);
+        $this->SetFont('Arial','B',15);
+        $this->cell(80);
+        $this->cell(30,10,'DIPLOMA',1,0,'C');
+
+    }
+    function Footer(){
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+
+$pdf = new PDF();
+$pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,$nombre.' '.$apellidos);
