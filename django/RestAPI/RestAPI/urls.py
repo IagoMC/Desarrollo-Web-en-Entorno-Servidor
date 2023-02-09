b53e3cf6f5f525a2c06854e2bf7d6570e5cc0d1f
IagoMC <113433698+IagoMC@users.noreply.github.com>
2023-02-09 11:19:02 +0100

    URL
    Te dejamos las url  de nuestros endpoints

--- django/RestAPI/RestAPI/urls.py
+++ django/RestAPI/RestAPI/urls.py
@@ -1,24 +1,14 @@
-"""RestAPI URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from pai import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', home.urls)
-
-
+    path('editar', views.editar_perfitl),
+    path('password', views.cambiar_contraseña),
+    path('getphographers', views.buscar_photograpers),
+    path('getagencies', views.buscar_agencies),
+    path('photographer', views.photographer),
+    path('agencia', views.agencia),
+    path('añadircomentariosfotografo', views.add_comment_and_rating),
+    path('añadircomentariosagencia', views.add_comment_and_rating),    
 ]
