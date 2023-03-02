ff66a0ef992aeb119d586f1da0294e3bd13b6d09
IagoMC <113433698+IagoMC@users.noreply.github.com>
2023-03-02 16:28:05 +0100

    Update views.py

--- django/RestAPI/pai/views.py
+++ django/RestAPI/pai/views.py
@@ -16,6 +16,7 @@ import random
 from django.contrib.auth import get_user_model
 from django.views.decorators.http import require_http_methods
 from django.conf import settings
+from myapp.views import csrf_failure
 
 
 csrf_failure_view = settings.CSRF_FAILURE_VIEW
