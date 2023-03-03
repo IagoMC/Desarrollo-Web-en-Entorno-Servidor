ac7ddc25866abb243ddd74f46b4a6d7f3a293623
Iago <imoralesc@fpcoruna.afundacion.org>
2023-03-03 21:14:11 +0100

    subir todo

--- django/RestAPI/RestAPI/urls.py
+++ django/RestAPI/RestAPI/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from pai import views
-from RestAPI.views import csrf_failure
-#from .views import getphographers
+#from RestAPI.views import csrf_failure
+#from .views import buscar_photograpers
 #from RestAPI.views import password
 #from RestAPI.views import users
 
 urlpatterns = [
  #   path('editar', views.editar_perfitl),
    # path('password', views.cambiar_contrasena),
-#    path('getphographers', views.buscar_photograpers),
-    path('user',views.create_user),
+    path('getphographers', views.buscar_photograpers),
+#    path('user',views.create_user),
 #    path('getagencies', views.buscar_agencies),
    # path('photographer', views.photograpers),
   #  path('agencia', views.agencia),
