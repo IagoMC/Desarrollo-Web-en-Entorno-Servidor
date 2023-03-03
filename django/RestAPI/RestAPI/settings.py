a93267eefd15b5fc1d1e4e05d1dae26b0fb25f05
IagoMC <113433698+IagoMC@users.noreply.github.com>
2023-03-03 22:04:59 +0100

    Update settings.py

--- django/RestAPI/RestAPI/settings.py
+++ django/RestAPI/RestAPI/settings.py
@@ -12,7 +12,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_FAILURE_VIEW = 'myapp.views.csrf_failure_view'
+CSRF_FAILURE_VIEW = 'pai.views.csrf_failure_view'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
