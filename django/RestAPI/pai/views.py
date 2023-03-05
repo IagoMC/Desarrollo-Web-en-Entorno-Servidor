3b31d63cb6bbba53d5141b7da59bcb686ccf4abb
Iago <imoralesc@fpcoruna.afundacion.org>
2023-03-05 18:23:53 +0100

    subir los errores

--- django/RestAPI/pai/views.py
+++ django/RestAPI/pai/views.py
@@ -135,41 +135,41 @@ def loguearse(request):
 def aComentaris(request, fotografo_id):
     if request.method == 'POST':
 
-    data = json.loads(request.body)
-    comentario = data.get('comentario')
-    rating = data.get('rating')
+		data = json.loads(request.body)
+		comentario = data.get('comentario')
+		rating = data.get('rating')
 
-    if not comentario or not rating:
-        return JsonResponse({'error': 'Faltan datos en el cuerpo'}, status=400)
+		if not comentario or not rating:
+			return JsonResponse({'error': 'Faltan datos en el cuerpo'}, status=400)
 
-    try:
-        fotografo = Fotografo.objects.get(id=fotofotografo_id_id)
-    except Fotografo.DoesNotExist:
-        return JsonResponse({'error': 'Fotógrafo no encontrado'}, status=404)
-
-
-    try:
-        usuario = Clientes.objects.get(token=token)
-    except Clientes.DoesNotExist:
-        try:
-            usuario = Fotografo.objects.get(token=token)
-        except Fotografo.DoesNotExist:
-            try:
-                usuario = Agencia.objects.get(token=token)
-            except Agencia.DoesNotExist:
-                return JsonResponse({'error': 'No autorizado'}, status=404)
+		try:
+			fotografo = Fotografo.objects.get(id=fotofotografo_id_id)
+		except Fotografo.DoesNotExist:
+			return JsonResponse({'error': 'Fotógrafo no encontrado'}, status=404)
 
 
+		try:
+			usuario = Clientes.objects.get(token=token)
+		except Clientes.DoesNotExist:
+			try:
+				usuario = Fotografo.objects.get(token=token)
+			except Fotografo.DoesNotExist:
+				try:
+					usuario = Agencia.objects.get(token=token)
+				except Agencia.DoesNotExist:
+					return JsonResponse({'error': 'No autorizado'}, status=404)
 
 
-    comentarioFotografo = ComentarioFotografo.objects.create(
-        id=ComentarioFotografo.objects.all()+1,
-        idUsuario=usuario.id,
-        idFotografo=fotografo_id,
-	comentario=comentario,
-        valoracion=rating,
-    )
-    return JsonResponse({'message': 'Comentario y valoración publicados'}, status=201)    
+
+
+		comentarioFotografo = ComentarioFotografo.objects.create(
+			id=ComentarioFotografo.objects.all()+1,
+			idUsuario=usuario.id,
+			idFotografo=fotografo_id,
+		comentario=comentario,
+			valoracion=rating,
+		)
+		return JsonResponse({'message': 'Comentario y valoración publicados'}, status=201)    
 """
 from array import array
 from django.db import models
